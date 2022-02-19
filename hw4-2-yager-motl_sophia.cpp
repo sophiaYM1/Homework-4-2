d3d4574754fe2e7b29506d3c4e992f6d1160f6bf
sophiaYM1 <98847010+sophiaYM1@users.noreply.github.com>
2022-02-19 15:22:10 -0500

    Update hw4-2-yager-motl_sophia.cpp

--- hw4-2-yager-motl_sophia.cpp
+++ hw4-2-yager-motl_sophia.cpp
@@ -3,43 +3,45 @@
 
 using namespace std;
 
-void find_data(str file_name)
+void find_data(file_name)
 {
-  map <string, num> file_data;
-  file_data['lines'] = 0;
-  file_data['word'] = 0;
-  file_data['characters'] = 0;
-  while(getline(filename, line))
+	//initialize map
+  	map <string, num> file_data;
+  	file_data['lines'] = 0;
+ 	file_data['word'] = 0;
+ 	file_data['characters'] = 0;
+	
+ 	string line;
+	
+  	while(getline(filename, line))
 	{
 		file_data['lines'] += 1;
-    file_name >> file_data['words'] += 1;
-    for (int index; index < line.length(), index++)
-    {
-      file_data['characters'] += 1;
-    }
+    		file_name >> file_data['words'] += 1;
+    		for (int index; index < line.length(), index++)
+    		{
+     			file_data['characters'] += 1;
+  		}
 	}
-	inputFile.close();
-
+	file_name.close();
 }
 
 
 int main(int argc, char *argv[])
 {
-	//Variables and input
- 	ifstream inputFile;
-		
-	if (argc < 3)
-	{
-		cerr << "Usage: " << argv[0] << " filename start_word" << endl;
-		return 1;
-	}
-	
 	ifstream text(argv[1]);
 	if(text.fail())
 	{
 		cerr << "Error opening the file.\n";
 		return 1;
 	}
-  else 
-    find_data(input_file);
-  return 0;
+	map <string, num> file_data;
+  	else 
+	{
+    		file_data = find_data(input_file);
+	}
+	cout << "Name of file" << setw(6) <<< text << endl;
+	cout << "Num of lines" << setw(6) << file_data[0] << endl;
+	cout << "Num of words" << setw(6) << file_data[1] << endl;
+	cout << "Num of characters" << setw(6) << file_data[2] << endl;
+	
+  	return 0;
