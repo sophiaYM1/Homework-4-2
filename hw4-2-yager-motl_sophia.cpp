fff25bd27d849f418a04bb4d1ae7cf5b931ab128
sophiaYM1 <98847010+sophiaYM1@users.noreply.github.com>
2022-02-19 21:14:04 -0500

    Update hw4-2-yager-motl_sophia.cpp

--- hw4-2-yager-motl_sophia.cpp
+++ hw4-2-yager-motl_sophia.cpp
@@ -45,15 +45,14 @@ int main(int argc, char *argv[])
 					file_data["characters"] += 1;
 				}
         		}
-			//file_data["characters"] += line.length();
   		}
 			cout << file_data << endl;
 	}
 	
-	string map = file_data.toString();
-	//string num_l = map.get(0);
-	//string num_2 = map.get(1);
-	//string num_3 = map.get(2);
+	//string map = file_data.toString();
+	string num_l = file_data.get(0);
+	string num_2 = file_data.get(1);
+	string num_3 = file_data.get(2);
 	//cout << "Name of file" << setw(6) << text << endl;
 	//cout << "Num of lines" << setw(6) << num_1 << endl;
 	//cout << "Num of words" << setw(6) << num_2 << endl;
